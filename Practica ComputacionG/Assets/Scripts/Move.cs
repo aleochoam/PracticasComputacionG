bdf60e8cb682a933050bc9dbd234587743ad097a
Alejandro <alejo8a_2@hotmail.com>
2016-03-18 15:29:57 -0500

    Cañon listo

---
+++ Practica ComputacionG/Assets/Scripts/Move.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+    float deltaRotation = 30f;
+    float deltaMovement = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Rotate();
+	}
+
+    void Rotate() {
+        if (Input.GetKey(KeyCode.UpArrow)){
+            transform.Rotate(new Vector2(0, deltaRotation) * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow)){
+            transform.Rotate(new Vector2(0, -deltaMovement) * Time.deltaTime);
+        }
+        
+    }
+}
