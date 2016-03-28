d0da1bfccc35d6c684cbfce8268f22a266187a16
Alejandro <alejo8a_2@hotmail.com>
2016-03-27 19:52:40 -0500

    Crear la bala

--- Practica ComputacionG/Assets/Scripts/Shooter.cs
+++ Practica ComputacionG/Assets/Scripts/Shooter.cs
@@ -3,11 +3,13 @@ using System.Collections;
 
 public class Shooter : MonoBehaviour {
 
+    public float damege = 0;
+    public float timeToFire = 0;
     public float fireRate = 0;
-    //public float damege = 0;
-    public LayerMask toHit;
 
-    float timeToFire = 0;
+    public LayerMask toHit;
+    public Transform Bala;
+   
     Transform PuntoDisparo;
     Transform BaseCanon;
 
@@ -23,27 +25,33 @@ public class Shooter : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        //Shoot();
+        //Shoot(100);
         if (fireRate == 0){
             if (Input.GetKeyDown(KeyCode.Space)){
-                Shoot();
+                float fuerza = 0;
+                /*while (Input.GetKeyDown(KeyCode.Space)) {
+                    fuerza+=10;
+
+                }*/
+                Shoot(100);
             }
         }
 	}
 
-    void Shoot(){
-        Debug.Log("Disparo pum piu");
+    void Shoot(float fuerza){
         Vector2 posicionBase = BaseCanon.position;
-        RaycastHit2D hit = Physics2D.Raycast(PuntoDisparo.position, PuntoDisparo.position - BaseCanon.position, 100, toHit);
-        Effect()
+        RaycastHit2D hit = Physics2D.Raycast(PuntoDisparo.position, PuntoDisparo.position - BaseCanon.position, fuerza, toHit);
+        Effect();
         Debug.DrawLine(posicionBase, (PuntoDisparo.position - BaseCanon.position) *100 , Color.cyan );
         if (hit.collider != null){
             Debug.DrawLine(PuntoDisparo.position, hit.point, Color.red);
         }
     }
 
-    void Effect()
-    {
+    void Effect(){
+        GameObject shot = Instantiate(Bala, PuntoDisparo.position, PuntoDisparo.rotation) as GameObject;
+        //shot.AddForce(new Vector3(10,0,0));
+        shot.transform.localScale += new Vector3(2, 2, 2);
 
     }
 }
